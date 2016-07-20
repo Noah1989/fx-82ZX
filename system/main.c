32b66e303e9706f76bd03c9672a2cde530e8ea4e
Stefan Noack <stefan@noack-software.de>
2016-07-21 01:10:33 +0200

    use provided include files from sdcc (just discovered lint.h)

--- system/main.c
+++ system/main.c
@@ -1,4 +1,7 @@
-#include "lib/8051.h"
+#include </usr/share/sdcc/include/mcs51/lint.h>
+#include </usr/share/sdcc/include/mcs51/8051.h>
+
+_LINT_H // suppress "unused import" warning in IDE
 
 int main() {
 
